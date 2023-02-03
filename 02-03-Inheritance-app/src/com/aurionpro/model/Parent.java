12de3ca3342eaff3f0c8ad4ad92987f890e54522
tejasprabhu <tejasprabhu782@gmail.com>
2023-02-03 17:13:10 +0530

    Inheritance Programs

---
+++ 02-03-Inheritance-app/src/com/aurionpro/model/Parent.java
@@ -0,0 +1,9 @@
+package com.aurionpro.model;
+
+public class Parent {
+	public Parent() {
+		System.out.println("Hello You are in Parent Class");
+		
+	}
+
+}
---
+++ 02-03-Inheritance-app/src/com/aurionpro/test/InheritanceTest.java
@@ -0,0 +1,12 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.ChildA;
+import com.aurionpro.model.Parent;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		Parent obj = new Parent();
+		// ChildA obj1=new ChildA();
+	}
+}
